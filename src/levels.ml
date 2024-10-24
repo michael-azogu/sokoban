d5524c1ab7cd3b65a710f26fbdbe84938e1c77ed
michael-azogu <michaelazogu101@gmail.com>
2024-10-24 18:10:49 +0100

    parser in the works

--- src/levels.ml
+++ src/levels.ml
@@ -54,3 +54,64 @@ let read_levels = '\\'
    NB: 0 = absent , 1 = present
 
 *)
+
+let parse bytes =
+  let nbytes = Bytes.length bytes in
+
+  if nbytes > 4 then (
+    let r, c, kr, kc =
+      ( Bytes.get_uint8 bytes 0,
+        Bytes.get_uint8 bytes 1,
+        Bytes.get_uint8 bytes 2,
+        Bytes.get_uint8 bytes 3
+      )
+    in
+
+    let reasons =
+      let because pred msg acc =
+        if pred = true then
+          msg :: acc
+        else
+          acc
+      in
+      let ord n =
+        let suffix =
+          let j, k = (n mod 10, n mod 100) in
+            if j = 1 && k <> 11 then
+              "st"
+            else if j = 2 && k <> 12 then
+              "nd"
+            else if j = 3 && k <> 13 then
+              "rd"
+            else
+              "th"
+        in
+          string_of_int n ^ suffix
+      in
+      let krth, kcth = (kr + 1, kc + 1) in
+        []
+        |> because (c = 0 || r = 0)
+           @@ sprintf
+                "- the warehouse cant have %d rows and %d columns"
+                r
+                c
+        |> because (krth > r)
+           @@ sprintf
+                "- the keeper's position on the (%s)row is beyond no-of-rows:(%d)"
+                (ord krth)
+                r
+        |> because (kcth > c)
+           @@ sprintf
+                "- the keeper's position on the (%s)column is beyond no-of-columns:(%d)"
+                (ord kcth)
+                c
+    in
+
+    if List.is_empty reasons then (
+      let tile_bytes = Bytes.sub bytes 2 nbytes in
+        ()
+    ) else
+      (* try with raise *)
+      printf "parsing terminated:\n%s\n" (String.concat "\n" reasons)
+  ) else
+    ()
