8327a5d31a3a580b83803ebcb08d41168054fb59
michael-azogu <michaelazogu101@gmail.com>
2024-10-28 18:41:11 +0100

    get screen resolution

--- src/utils.ml
+++ src/utils.ml
@@ -1,3 +1,5 @@
+open Core
+
 let ord n =
   let suffix =
     let j, k = (n mod 10, n mod 100) in
@@ -11,3 +13,17 @@ let ord n =
         "th"
   in
     string_of_int n ^ suffix
+
+let get_resolution () =
+  let ic =
+    Core_unix.open_process_in "xrandr | grep '*' | awk '{print $1}'"
+  in
+  let resolution = In_channel.input_line ic in
+    In_channel.close ic ;
+    match resolution with
+    | Some r -> (
+      match String.split ~on:'x' r with
+      | [ w; h ] -> (int_of_string w, int_of_string h)
+      | _ -> (0, 0)
+    )
+    | None -> (0, 0)
